6429da67086f1491134e5753898da6182b03bbab
Thanhhff <thanhnguyen200399@gmail.com>
2019-07-11 22:12:20 +0700

    Truy cap mang 1 chieu

---
+++ Python cho DSTT/truy_cap_mang_1chieu.py
@@ -0,0 +1,20 @@
+import numpy as np 
+
+# Kich thuoc cua mang
+# Kích thước của một mảng numpy x nói chung được xác định bằng numpy.array.shape
+x = np.array([3, 4, 5, 10])
+print(x.shape)
+
+d = x.shape[0]
+print(d)
+
+# Chi so
+print("\n--------------------")
+print(x[0])
+
+# Chi so nguoc
+
+print(x[d-1] - x[-1])
+print(x[-2])
+
+
