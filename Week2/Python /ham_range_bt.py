aacd6133390741890f062c51634bfc765e1b2ff2
Thanhhff <thanhnguyen200399@gmail.com>
2019-07-11 22:41:28 +0700

    upate

--- Week2/Python /ham_range_bt.py
+++ Week2/Python /ham_range_bt.py
@@ -2,7 +2,7 @@ for num in range(0, 10):
     for i in range(2, num):
         if num % i == 0:
             j = num / i
-            print("%d bang %d * %d" %(num, i, j))
+            print("%d bang %d * %d" % (num, i, j))
             break
-        else:
-            print(num, "la so nguyen to")
+    else:
+        print(num, "la so nguyen to")
