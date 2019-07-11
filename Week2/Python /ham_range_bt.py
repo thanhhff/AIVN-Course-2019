0ada0e27bd6bc707a41b882604b8392ff651a96c
Thanhhff <thanhnguyen200399@gmail.com>
2019-07-11 22:36:55 +0700

    bai tap range

---
+++ Week2/Python /ham_range_bt.py
@@ -0,0 +1,8 @@
+for num in range(0, 10):
+    for i in range(2, num):
+        if num % i == 0:
+            j = num / i
+            print("%d bang %d * %d" %(num, i, j))
+            break
+        else:
+            print(num, "la so nguyen to")
