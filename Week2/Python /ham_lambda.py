d93ed624d025e56de2a4cdaab33f41d21e8ab550
Thanhhff <thanhnguyen200399@gmail.com>
2019-07-12 11:15:30 +0700

    lambda

--- Week2/Python /ham_lambda.py
+++ Week2/Python /ham_lambda.py
@@ -1,5 +1,12 @@
+#Lambda with filter
 list_goc = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
 list_moi = list(filter(lambda a: (a % 2 == 0), list_goc))
 
 print(list_moi)
+
+# Lambda with Map
+
+list_moi2 = list(map(lambda a: a*10, list_goc))
+
+print(list_moi2)
